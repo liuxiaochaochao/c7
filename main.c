9a725b3be476c54bb214924806cd024426d94a3b
chao <chao@qq.com>
2019-01-18 14:37:14 +0800

    功能345

--- main.c
+++ main.c
@@ -6,10 +6,11 @@ struct xuesheng
     int yingyu;
     char name[1000];
 };
+int sum = 0;
 
 int main()
 {
-    int index=0;
+    int index = 0;
     struct xuesheng arr[1000];
 
     printf("进入系统（三科成绩）");
@@ -27,23 +28,23 @@ int main()
         int a;
         scanf("%d", &a);
 
-        if (a ==1)
+        if (a == 1)
         {
             printf("请输入姓名：\n");
-            scanf("%s",&(arr[index].name));
+            scanf("%s", &(arr[index].name));
             printf("请输入数学：\n");
-            scanf("%d",&(arr[index].shuxue));
+            scanf("%d", &(arr[index].shuxue));
             printf("请输入语文：\n");
-            scanf("%d",&(arr[index].yuwen));
+            scanf("%d", &(arr[index].yuwen));
             printf("请输入英语：\n");
-            scanf("%d",&(arr[index].yingyu));
+            scanf("%d", &(arr[index].yingyu));
             index++;
             printf("点击回车继续");
             char x;
-            scanf("%c",&x);
-            scanf("%c",&x);
+            scanf("%c", &x);
+            scanf("%c", &x);
         }
-        if (a ==2)
+        if (a == 2)
         {
             if (index > 0)
             {
@@ -59,24 +60,50 @@ int main()
                 char x;
                 scanf("%c", &x);
                 scanf("%c", &x);
-            }            
+            }
         }
-        if (a ==3)
+        if (a == 3)
+        {
+
+            for (int i = 0; i < index; i++)
+            {
+                printf("学生姓名%s\n", arr[i].name);
+                printf("学生语文%d\n", arr[i].yuwen);
+                printf("学生数学%d\n", arr[i].shuxue);
+                printf("学生英语%d\n", arr[i].yingyu);
+            }
+        }
+        if (a == 4)
+        {
+
+            for (int i = 0; i < index; i++)
+            {
+                int sum = 0;
+                sum = arr[i].yuwen + arr[i].shuxue + arr[i].yingyu;
+                if (sum < 180)
+                {
+                    printf("%d%s", i + 1, arr[i].name);
+                }
+            }
+        }
+        if (a == 5)
+        {
+
+            for (int i = 0; i < index; i++)
+            {
+                if (arr[i].yuwen < 60 && arr[i].shuxue < 60 && arr[i].yingyu)
+                {
+                    printf("姓名%s\n", arr[i].name);
+                }
+            }
+        }
+        if (a == 6)
         {
         }
-        if (a ==4)
+        if (a == 7)
         {
         }
-        if (a ==5)
-        {
-        }
-        if (a ==6)
-        {
-        }
-        if (a ==7)
-        {
-        }
-        if (a ==8)
+        if (a == 8)
         {
             printf("点击回车继续\n");
             break;
